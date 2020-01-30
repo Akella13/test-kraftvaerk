1cf6a6eb74829ababd326795310ffde037c15230
Arman Kozhakhmetov <kozhakhmetov13@gmail.com>
2020-01-30 22:34:27 +0300

    fix: browsersync refreshing because of self-closed tag

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,3 +1,13 @@
 let mix = require('laravel-mix');
-mix.js('src/js/app.js', 'public/js/script.js')
-    .sass('src/scss/app.scss', 'public/css/style.css');
+
+mix.js('src/js/app.js', 'public/js/script.js');
+
+mix.sass('src/scss/app.scss', 'public/css/style.css');
+
+mix.browserSync({
+    proxy: false,
+    server: true,
+    watch: true,
+});
+
+mix.disableNotifications();
