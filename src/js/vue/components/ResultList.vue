54524687fa657a2c68012d9cf2abd772f755d90a
Arman Kozhakhmetov <kozhakhmetov13@gmail.com>
2020-02-08 13:25:04 +0300

    feat: infinite scroll directive

--- src/js/vue/components/ResultList.vue
+++ src/js/vue/components/ResultList.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <ul>
+    <ul v-infinite-scroll="loadMore" infinite-scroll-disabled="busy" infinite-scroll-distance="10">
       <li v-for="(item,index) in result" :key="index">
         {{ item.Name }}
         <img :src="`data:image/png;base64,${item.Logo}`">
@@ -12,11 +12,15 @@
 <script>
 import axios from 'axios';
 import { mapState, mapGetters } from 'vuex';
+import infiniteScroll from 'vue-infinite-scroll';
 
 export default {
+  directives: { infiniteScroll },
   data() {
     return {
-      result: null,
+      result: [],
+      nextPage: 1,
+      busy: false,
     }
   },
   computed: {
@@ -26,26 +30,36 @@ export default {
   },
   watch: {
     params() {
+      // reset result while pulling new data
+      this.result = [];
+      this.nextPage = 1;
       this.pullResults(1);
     }
   },
   methods: {
     pullResults(pageNumber) {
+      this.busy = true;
       axios({
         method: 'get',
         url: `/public/data/pages/${this.params.SortField}/${pageNumber}.json`,
         params: this.params,
       })
         .then((response) => {
-          this.result = null;
           // fake ajax response timeout
           setTimeout(() => {
-            this.result = response.data;
+            this.result.push(...response.data);
+            this.nextPage += 1;
+            this.busy = false;
           }, 1000);
         })
         .catch((error) => {
           console.error(error.response.data);
         })
+    },
+    loadMore: function() {
+      if (this.nextPage < 7) {
+        this.pullResults(this.nextPage);
+      }
     },
   }
 }
